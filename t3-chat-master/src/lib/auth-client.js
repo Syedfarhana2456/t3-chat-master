39f688de25ca9093cd90670474357051d9cda518
SYED FARHANA <syedfarhana2456@gmail.com>
2026-09-06 23:45:28 +0530

    Delete t3-chat-master directory

--- t3-chat-master/src/lib/auth-client.js
+++
@@ -1,8 +0,0 @@
-import { createAuthClient } from "better-auth/react";
-
-export const { signIn, signUp, useSession, signOut } = createAuthClient({
-  baseURL:
-    process.env.NODE_ENV === "production"
-      ? process.env.NEXT_PUBLIC_APP_URL
-      : "http://localhost:3000",
-});
